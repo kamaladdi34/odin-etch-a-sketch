d681b2343fbc445f9b06c1c42817ebb54cfd93d4
Kamal Addi <kamal.addi.dev@gmail.com>
2023-06-08 22:51:19 +0100

    Optimize updateGridSize function

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const pixelContainer = document.querySelector('.pixel-container');
 const containerHeight = pixelContainer.clientHeight;
 const sizeSLider = document.querySelector('.grid-size-slider > input');
+let currentPixelAmount = 0;
 let pixels = [];
 sizeSLider.addEventListener('input',(e)=>{updateGridSize(sizeSLider.value)});
 function createPixel(gridSize){
@@ -19,15 +20,23 @@ createPixelPpool(100);
 updateGridSize(20);
 function updateGridSize(gridSize)
 {
+    const pixelAmount = gridSize*gridSize;
+    let pixelsHeight = 0, pixelsHeightShown = 0,pixelsDisabled =0 ;
     for (let i = 0; i < pixels.length; i++) {
-        if(i<gridSize*gridSize)
-        {
+        if(i<pixelAmount && i < currentPixelAmount){
+            pixels[i].style.height = pixels[i].style.width = `${containerHeight/gridSize}px`;
+            pixelsHeight++;
+        }
+        else if(i<pixelAmount){
             pixels[i].classList.remove('hidden');
             pixels[i].style.height = pixels[i].style.width = `${containerHeight/gridSize}px`;
-        }
-        else
-        {
+            pixelsHeightShown++;
+        }else{
             pixels[i].classList.add('hidden');
+            pixelsDisabled++;
         }
     }
+    currentPixelAmount = pixelAmount;
+    console.log(`pixels with size updated:${pixelsHeight}\npixels enabled and size updated: ${pixelsHeightShown}\npixels disabled: ${pixelsDisabled}`);
+    pixelsDisabled = pixelsHeight = pixelsHeightShown = 0;
 }
